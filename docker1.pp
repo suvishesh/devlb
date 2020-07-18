8a8fa8902e0ab9289a8c90f11ca879eeb0647516
suvishesh <marksuvi89@gmail.com>
2020-07-18 19:32:29 +0000

    module added

--- docker1.pp
+++ docker1.pp
@@ -1,18 +1,6 @@
-package { 'docker':
-ensure => present,
-}
-service { 'docker':
-ensure => running,
-require => Package['docker'],
-}
-exec { 'rm':
-command => '/bin/docker rm -f $(sudo docker ps -a -q)',
-before => Exec['build'],
-}
-exec { 'build':
-command => '/bin/docker build . -t website',
-}
-exec { 'run':
-command => '/bin/docker run -it -d -p 82:80 -d website',
-require => Exec['build'],
-}
+node default {
+
+include docker
+
+ }
+
---
+++ init0.pp
@@ -0,0 +1,34 @@
+class docker {
+package { 'docker':
+ensure => present,
+}
+service { 'docker':
+ensure => running,
+require => Package['docker'],
+}
+file { "/root/Dockerfile":
+        
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/docker/Dockerfile',
+    }
+
+file { "/root/index.html":
+        
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/docker/index.html',
+    }
+exec { 'rm':
+command => '/bin/docker rm -f $(sudo docker ps -a -q)',
+before => Exec['build'],
+}
+exec { 'build':
+command => '/bin/docker build . -t website',
+}
+exec { 'run':
+command => '/bin/docker run -it -d -p 82:80 -d website',
+require => Exec['build'],
+}
+}
+
