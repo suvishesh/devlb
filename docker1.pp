74eabdb57c86703649e6c2582186470d205bdde5
suvishesh <marksuvi89@gmail.com>
2020-07-18 14:57:44 +0000

    config added

---
+++ docker1.pp
@@ -0,0 +1,18 @@
+package { 'docker':
+ensure => present,
+}
+service { 'docker':
+ensure => running,
+require => Package['docker'],
+}
+exec { 'rm':
+command => '/bin/docker rm -f $(sudo docker ps -a -q)',
+before => Exec['build'],
+}
+exec { 'build':
+command => '/bin/docker build . -t website',
+}
+exec { 'run':
+command => '/bin/docker run -it -d -p 82:80 -d website',
+require => Exec['build'],
+}
